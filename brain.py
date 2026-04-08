2bdc553eb858ac748f29f4c37565f4d68789a516
goku6276 <23401080070125@cecytea.edu.mx>
2026-04-07 18:36:11 -0600

    Update brain.py

--- brain.py
+++ brain.py
@@ -1,5 +1,6 @@
 from math_engine import solve
 from ai_engine import generate
+from moderation import handle_user
 from medical_engine import detect_medical, medical_response
 def detectar_tipo(texto):
     texto = texto.lower()
@@ -14,9 +15,17 @@ def detectar_tipo(texto):
      #Medico
     if any(x in texto for x in["dolor", "fiebre", "tos","gripa", "gripe", "enfermo", "síntomas","sintomas","medicina","medico","doctor","cabeza","estómago","estomago","náusea","nausea", "mareo","vomito","vomitar","diarrea","covid","temperatura"]):
         return "medico"
-        
+    if any(x in texto for x in["idiota","estúpido","estupido","tonto","imbécil","imbecil","pendejo","pendeja","mierda","puta","puto","vete a la","callate","cállate","no sirves","eres basura"]):
+    return ""
     return "normal"
 
+def process(user, message):
+
+    # moderación primero
+    warning = handle_user(user, message)
+
+    if warning:
+        return warning
 
 def process(user, message):
 
